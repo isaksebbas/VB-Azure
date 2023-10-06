d6dd04f97452d44e47bf363f5f49eece20500147
isaksebbas <isak.sebbas@arcada.fi>
2023-10-06 22:21:26 +0300

    Not ideal but testing

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    databaseURL: 'postgres://gkrlsmwl:DiTG5iL7WHZD4seU4TcjrFvpeNVz7tmO@snuffleupagus.db.elephantsql.com/gkrlsmwl',
+    databaseUsername: 'gkrlsmwl',
+    databasePassword: 'DiTG5iL7WHZD4seU4TcjrFvpeNVz7tmO',
+  };
+  
