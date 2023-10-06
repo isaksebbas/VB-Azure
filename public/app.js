57656f094b7ff1d042cb7ace2a1fd00545a26484
isaksebbas <isak.sebbas@arcada.fi>
2023-10-06 22:47:10 +0300

    Change of WSS address

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 console.log("App.js loaded");
 
 //const socket = new WebSocket('ws://localhost:3000'); // Replace 'your-server-url' with your actual WebSocket server URL
-const socket = new WebSocket('wss://20.107.224.52:3000');
+const socket = new WebSocket('wss://w-o-m-2023.azurewebsites.net');
 
 // Function to send a message over the WebSocket
 function sendMessage(type, data) {
